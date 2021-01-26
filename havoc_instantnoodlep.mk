ad2beabfcc3627da0d96fb9b8c17146e20860252
cjh1249131356 <cjh1249131356@gmail.com>
2021-01-26 11:15:16 +0800

    instantnoodlep: Havoc bringup

--- havoc_instantnoodlep.mk
+++ havoc_instantnoodlep.mk
@@ -21,10 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_instantnoodlep
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+# Gapps Config
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_NAME := havoc_instantnoodlep
 PRODUCT_DEVICE := instantnoodlep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
